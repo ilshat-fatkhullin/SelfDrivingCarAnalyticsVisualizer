d2e01f521b8c2c2cfb89647c1f888ce9b8e6cb42
Ilshat Fatkhullin <ilshat.fatkhullin@gmail.com>
2020-02-12 11:44:52 +0300

    Added timeline labels.

---
+++ SelfDrivingCarAnalyticsVisualizer/Assets/Scripts/UI/TimelineLabels.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimelineLabels : MonoBehaviour
+{
+    [SerializeField]
+    private int _labelsCount;
+
+    [SerializeField]
+    private GameObject _timelineLabel;
+
+    private List<GameObject> _timelineLabels;
+
+    private RectTransform _rectTransform;
+
+    private void Start()
+    {
+        EventBus.Instance.OnTimelineChangeEvent.AddListener(OnTimelineChange);
+
+        _timelineLabels = new List<GameObject>();
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void OnTimelineChange(float duration)
+    {
+        while (_timelineLabels.Count > 0)
+        {
+            Destroy(_timelineLabels[0]);
+            _timelineLabels.RemoveAt(0);
+        }
+
+        float step = duration / (_labelsCount - 1);
+        float time = 0;
+
+        for (int i = 0; i < _labelsCount; i++)
+        {
+            GameObject label = Instantiate(_timelineLabel, transform);
+            label.GetComponent<Text>().text = GetTimelineLabelFromTime(time);
+            label.GetComponent<RectTransform>().anchoredPosition = 
+                new Vector2(Mathf.Lerp(0, _rectTransform.rect.width, time / duration), 0);
+            _timelineLabels.Add(label);
+            time += step;
+        }
+    }
+
+    private string GetTimelineLabelFromTime(float time)
+    {
+        int seconds = Mathf.FloorToInt(time % 60);
+        int minutes = Mathf.FloorToInt(time / 60);
+        return string.Format("{0:D2}", minutes) + ":" + string.Format("{0:D2}", seconds);
+    }
+}
